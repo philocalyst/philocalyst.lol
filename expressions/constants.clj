3b1d7fe63358d663367c617bdb6975a153823e66
Myles Wirth <regime_skylark.0y@icloud.com>
2026-03-08 13:35:45 -0400

    Page routing

--- expressions/constants.clj
+++ expressions/constants.clj
@@ -6,7 +6,7 @@
 
 (def nav-items
   [{:href "/now" :text "now"}
-   {:href "/about" :text "about"}
+   {:href "/" :text "about"} ; The about page is our root
    {:href "/writings" :text "writings"}
    {:href "/photos" :text "photos"}
    {:href "/projects" :text "projects"}])
---
+++ pages/Projects/index.md
@@ -0,0 +1,3 @@
+## Projects
+
+- Emoji Picker
